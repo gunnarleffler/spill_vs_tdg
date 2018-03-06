d53c987055e7ee4d6ac5b1bf727d1bc2afe38c4f
jetilton <jfftilton@gmail.com>
2018-03-06 11:05:08 -0800

    requested cwms data in PST and put back in cwms in GMT after calcs

--- script/12hr/get_avgs.py
+++ script/12hr/get_avgs.py
@@ -58,6 +58,7 @@ def oregon_method(df:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
         .groupby(pd.Grouper(level='date', freq='D')) \
         .aggregate(lambda x: x.sort_values(ascending = False) \
         .reset_index(drop = True).iloc[:12].mean())
+    #set time to 1200 PST in GMT
     orgn.index = set_time_index(orgn.index, hour_int = 20)
     orgn.index.name = 'date'
     return orgn
@@ -93,7 +94,7 @@ def washington_method(df:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     roll = df.rolling(window = 12, min_periods = 1, axis = 0).mean()
     
     wa_daily_max = roll.groupby(pd.Grouper(level='date', freq='D')).max()
-    
+    #set time to 1200 PST in GMT
     wa_daily_max.index = set_time_index(wa_daily_max.index, hour_int = 20)
     wa_daily_max.index.name = 'date'
     return wa_daily_max
@@ -190,7 +191,7 @@ if __name__ == "__main__":
         path_end = value['path']
         paths.append('{}{}{}'.format(key, path, path_end))
     
-    df = get_cwms(paths, lookback = lookback, fill = False, public = True, timezone = 'GMT')
+    df = get_cwms(paths, lookback = lookback, fill = False, public = True, timezone = 'PST')
     meta = df.__dict__['metadata']
     data_dict = {'Washington': [x+'_%_Saturation_TDG' for x in washington],
                  'Oregon': [x+'_%_Saturation_TDG' for x in oregon],
@@ -216,7 +217,7 @@ if __name__ == "__main__":
                                                 x.dropna(), 
                                                 x.name.split('_')[0]+path_name +  meta[x.name]['path'].split('-')[-1], 
                                                 meta[x.name]['units'], 
-                                                meta[x.name]['timezone']), axis = 0)
+                                                'GMT'), axis = 0)
         
         if rawJSON:instapost.apply(lambda x: print(json.dumps(x)+"\n---"))
         else: instapost.apply(lambda x: print(yaml.dump(x)))
